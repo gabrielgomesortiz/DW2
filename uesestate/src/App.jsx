7b88a40d7fbe40db99132a1e799ff0f1d6fccdf5
gabrielgomesortiz <gabrielgomesortiz@gmail.com>
2026-04-24 09:39:00 -0300

    feito

--- uesestate/src/App.jsx
+++ uesestate/src/App.jsx
@@ -7,20 +7,37 @@ function App() {
     setContador(contador + 1)
   }
   const soma2 = () => {
-    setContador((contador) => contador + 1)
-    setContador((contador) => contador + 1)
-    setContador((contador) => contador + 1)
+    contador == 0 ?
+      setContador(contador + 1 * 3)
+      :
+      setContador(contador * 3)
+  }
+  const diferenca = () => {
+    contador != 0 ?
+      setContador(contador - 1)
+      :
+      alert("não pode")
+
+  }
+  const zerar = () => {
+    contador != 0 ?
+      setContador(0)
+      :
+      alert("não pode")
+
   }
 
 
-return (
-  <>
-    <h1>{contador}</h1>
-    <button onClick={soma}>implementa 1x</button>
-    <button onClick={soma2}>implementa 3x</button>
+  return (
+    <>
+      <h1>{contador}</h1>
+      <button onClick={soma}>implementa 1x</button>
+      <button onClick={soma2}>implementa 3x</button>
+      <button onClick={diferenca}> diferença</button>
+      <button onClick={zerar}>zerar contador</button>
 
-  </>
-)
+    </>
+  )
 }
 
 export default App
